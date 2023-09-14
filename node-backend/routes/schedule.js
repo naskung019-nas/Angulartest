f9ead777f60b720de3fe15ad20a6ebd44477fecd
acnologia96 <134404081+acnologia96@users.noreply.github.com>
2023-09-14 12:19:39 +0700

    put post getid

--- node-backend/routes/schedule.js
+++ node-backend/routes/schedule.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { get_schedule,get_schedule_id } = require("../controllers/schedule");
+const { get_schedule,get_schedule_id,createSupportSchedule,updateSupportSchedule } = require("../controllers/schedule");
 
 router.get("/get_schedule", get_schedule);
 router.get("/get_schedule/:id", get_schedule_id);
+router.post("/create_schedule",createSupportSchedule);
+router.put("/put_schedule/:id",updateSupportSchedule);
 
 module.exports = router;
